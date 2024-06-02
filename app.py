e39ed56ca294350f69ecd0a72661b81b74690c2e
Hiep Do Trong <dotronghiephd@gmail.com>
2024-06-02 09:29:19 +0700

    add file

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+from collections import Counter
+import re
+
+app = Flask(__name__)
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+
+def ngram(token, n): 
+    output = []
+    for i in range(n-1, len(token)): 
+        ngram = ' '.join(token[i-n+1:i+1])
+        output.append(ngram) 
+    return output
+
+def create_feature(text, nrange=(1, 1)):
+    text_features = [] 
+    text = text.lower() 
+    text_alphanum = re.sub('[^a-z0-9#]', ' ', text)
+    for n in range(nrange[0], nrange[1]+1): 
+        text_features += ngram(text_alphanum.split(), n)    
+    text_punc = re.sub('[a-z0-9]', ' ', text)
+    text_features += ngram(text_punc.split(), 1)
+    return Counter(text_features)
+
+emoji_dict = {"joy":"😂", "fear":"😱", "anger":"😠", "sadness":"😢", "disgust":"😒", "shame":"😳", "guilt":"😳"}
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_text = request.json.get('text', '')
+    features = create_feature(input_text, nrange=(1, 4))
+    features = vectorizer.transform(features)
+    prediction = model.predict(features)[0]
+    output = input_text + "\n" + prediction + emoji_dict[prediction]
+    return jsonify({'prediction': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
